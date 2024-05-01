0f2e3f17c6035ce0c7d11acd96c0a8fd2f1de1a9
kripa-karthik <kripa1352001@gmail.com>
2024-05-01 17:04:37 +0530

    delted everything related to vercel

--- server.js
+++ server.js
@@ -1,75 +1,74 @@
 const express = require('express');
-const home=require("./routes/home")
-// const cors = require('cors');
-// const fs = require("fs");
-// const { parse } = require("csv-parse");
-// const { v4: uuid } = require('uuid');
+const cors = require('cors');
+const fs = require("fs");
+const { parse } = require("csv-parse");
+const { v4: uuid } = require('uuid');
 
-// const data = [];
+const data = [];
 
-//:\Users\Lenovo\FULL_STACK_PROJECTS\Movie-App-Backend
-
-// fs.createReadStream("C:\Users\Lenovo\FULL_STACK_PROJECTS\Movie-App-Backend\movies.csv")
-//   .pipe(
-//     parse({
-//       delimiter: ",",
-//       columns: true,
-//       ltrim: true,
-//     })
-//   )
-//   .on("data", function(row) {
-//     data.push(row);
-//   })
-//   .on("error", function(error) {
-//     console.log(error.message);
-//   })
-//   .on("end", function() {
-//     console.log("parsed csv data:");
-//     // console.log(data);
-//   });
+fs.createReadStream("./movies.csv")
+  .pipe(
+    parse({
+      delimiter: ",",
+      columns: true,
+      ltrim: true,
+    })
+  )
+  .on("data", function(row) {
+    data.push(row);
+  })
+  .on("error", function(error) {
+    console.log(error.message);
+  })
+  .on("end", function() {
+    console.log("parsed csv data:");
+    // console.log(data);
+  });
 
 const app = express();
 app.use(express.json());
-// app.use(cors())
+app.use(cors());
 
-app.use("/home",home);
+app.get('/', (req, res) => {
+  res.send('Hello Express app!')
+});
 
-// app.get('/api/movies', (req, res) => {
-//   console.log('movies data');
-//   res.json(data)
-// });
+app.get('/api/movies', (req, res) => {
+  console.log('movies data');
+  res.json(data)
+});
 
-// app.get('/api/movies/search', (req, res) => {
-//   const query = req.query.s;
-//   const movie = data.filter(({ name, director_name, writter_name, cast_name }) => name.toLowerCase() === query.toLowerCase() || director_name.toLowerCase() === query.toLowerCase() || writter_name.split(",").find(name => name.toLowerCase() === query.toLowerCase()) || cast_name.split(",").find(name => name.toLowerCase() === query.toLowerCase()));
-//   res.json(movie);
-// })
+app.get('/api/movies/search', (req, res) => {
+  const query = req.query.s;
+  const movie = data.filter(({ name, director_name, writter_name, cast_name }) => name.toLowerCase() === query.toLowerCase() || director_name.toLowerCase() === query.toLowerCase() || writter_name.split(",").find(name => name.toLowerCase() === query.toLowerCase()) || cast_name.split(",").find(name => name.toLowerCase() === query.toLowerCase()));
+  res.json(movie);
+})
 
-// app.get('/api/movies/genre', (req, res) => {
-//   const query = req.query.g;
-//   const movies = data.filter(({ genre }) => genre === query);
-//   res.json(movies);
-// })
+app.get('/api/movies/genre', (req, res) => {
+  const query = req.query.g;
+  const movies = data.filter(({ genre }) => genre === query);
+  res.json(movies);
+})
 
-// app.get('/api/movies/rating', (req, res) => {
-//   const query = req.query.i;
-//   const movies = data.filter(({ imdb_rating }) => imdb_rating >= query);
-//   res.json(movies);
-// })
+app.get('/api/movies/rating', (req, res) => {
+  const query = req.query.i;
+  const movies = data.filter(({ imdb_rating }) => imdb_rating >= query);
+  res.json(movies);
+})
 
-// app.get('/api/movies/genres', (req, res) => {
-//   const genres = data.reduce((acc, cur) => {
-//     acc = [...acc, { id: uuid(), genre: cur.genre.split(",")[0] }]
-//     return acc
-//   }, [])
-//   console.log({ genres });
-//   const allGenres = genres.flatMap(movie => movie.genre);
-//   const uniqueGenres = [...new Set(allGenres)];
-//   console.log({ uniqueGenres });
-//   const genres1 = uniqueGenres.map(genre => ({ id: uuid(), genre }));
-//   console.log({ genres1 })
-//   res.json(genres1);
-// })
+app.get('/api/movies/genres', (req, res) => {
+  const genres = data.reduce((acc, cur) => {
+    acc = [...acc, { id: uuid(), genre: cur.genre.split(",")[0] }]
+    return acc
+  }, [])
+  console.log({ genres });
+  const allGenres = genres.flatMap(movie => movie.genre);
+  const uniqueGenres = [...new Set(allGenres)];
+  console.log({ uniqueGenres });
+  const genres1 = uniqueGenres.map(genre => ({ id: uuid(), genre }));
+  console.log({ genres1 })
+  res.json(genres1);
+})
  
 const port=process.env.PORT || 3000;
 app.listen(port, () => {
